ac3d57f6e26b22b102a64e74e1691c9637ffb5a1
Ruslan Sennov <ruslan.sennov@gmail.com>
2017-09-15 08:33:57 +0300

    tuples components & tests

--- src/main/kotlin/io/vavr/kotlin/Tuples.kt
+++ src/main/kotlin/io/vavr/kotlin/Tuples.kt
@@ -149,4 +149,37 @@ operator fun <T1, T2, T3> Tuple3<T1, T2, T3>.component1(): T1 = this._1
 operator fun <T1, T2, T3> Tuple3<T1, T2, T3>.component2(): T2 = this._2
 operator fun <T1, T2, T3> Tuple3<T1, T2, T3>.component3(): T3 = this._3
 
-// TODO: define components for Tuple4-8
+operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component1(): T1 = this._1
+operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component2(): T2 = this._2
+operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component3(): T3 = this._3
+operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component4(): T4 = this._4
+
+operator fun <T1, T2, T3, T4, T5> Tuple5<T1, T2, T3, T4, T5>.component1(): T1 = this._1
+operator fun <T1, T2, T3, T4, T5> Tuple5<T1, T2, T3, T4, T5>.component2(): T2 = this._2
+operator fun <T1, T2, T3, T4, T5> Tuple5<T1, T2, T3, T4, T5>.component3(): T3 = this._3
+operator fun <T1, T2, T3, T4, T5> Tuple5<T1, T2, T3, T4, T5>.component4(): T4 = this._4
+operator fun <T1, T2, T3, T4, T5> Tuple5<T1, T2, T3, T4, T5>.component5(): T5 = this._5
+
+operator fun <T1, T2, T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6>.component1(): T1 = this._1
+operator fun <T1, T2, T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6>.component2(): T2 = this._2
+operator fun <T1, T2, T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6>.component3(): T3 = this._3
+operator fun <T1, T2, T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6>.component4(): T4 = this._4
+operator fun <T1, T2, T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6>.component5(): T5 = this._5
+operator fun <T1, T2, T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6>.component6(): T6 = this._6
+
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component1(): T1 = this._1
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component2(): T2 = this._2
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component3(): T3 = this._3
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component4(): T4 = this._4
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component5(): T5 = this._5
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component6(): T6 = this._6
+operator fun <T1, T2, T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7>.component7(): T7 = this._7
+
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component1(): T1 = this._1
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component2(): T2 = this._2
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component3(): T3 = this._3
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component4(): T4 = this._4
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component5(): T5 = this._5
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component6(): T6 = this._6
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component7(): T7 = this._7
+operator fun <T1, T2, T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>.component8(): T8 = this._8
--- src/test/kotlin/io/vavr/kotlin/TuplesKtTest.kt
+++ src/test/kotlin/io/vavr/kotlin/TuplesKtTest.kt
@@ -1,8 +1,8 @@
 package io.vavr.kotlin
 
+import io.vavr.*
+import io.vavr.collection.Seq
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class TuplesKtTest {
     @Test
@@ -10,74 +10,262 @@ class TuplesKtTest {
     }
 
     @Test
-    fun tuple1() {
+    fun tupleOf1() {
+        val t: Tuple1<Int> = tuple(1)
+        assert(t._1 == 1)
     }
 
     @Test
-    fun tuple2() {
+    fun tupleOf2() {
+        val t: Tuple2<Int, Int> = tuple(1, 2)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
     }
 
     @Test
-    fun tuple3() {
+    fun tupleOf3() {
+        val t: Tuple3<Int, Int, Int> = tuple(1, 2, 3)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
+        assert(t._3 == 3)
     }
 
     @Test
-    fun tuple4() {
+    fun tupleOf4() {
+        val t: Tuple4<Int, Int, Int, Int> = tuple(1, 2, 3, 4)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
+        assert(t._3 == 3)
+        assert(t._4 == 4)
     }
 
     @Test
-    fun tuple5() {
+    fun tupleOf5() {
+        val t: Tuple5<Int, Int, Int, Int, Int> = tuple(1, 2, 3, 4, 5)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
+        assert(t._3 == 3)
+        assert(t._4 == 4)
+        assert(t._5 == 5)
     }
 
     @Test
-    fun tuple6() {
+    fun tupleOf6() {
+        val t: Tuple6<Int, Int, Int, Int, Int, Int> = tuple(1, 2, 3, 4, 5, 6)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
+        assert(t._3 == 3)
+        assert(t._4 == 4)
+        assert(t._5 == 5)
+        assert(t._6 == 6)
     }
 
     @Test
-    fun tuple7() {
+    fun tupleOf7() {
+        val t: Tuple7<Int, Int, Int, Int, Int, Int, Int> = tuple(1, 2, 3, 4, 5, 6, 7)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
+        assert(t._3 == 3)
+        assert(t._4 == 4)
+        assert(t._5 == 5)
+        assert(t._6 == 6)
+        assert(t._7 == 7)
     }
 
     @Test
-    fun tuple8() {
+    fun tupleOf8() {
+        val t: Tuple8<Int, Int, Int, Int, Int, Int, Int, Int> = tuple(1, 2, 3, 4, 5, 6, 7, 8)
+        assert(t._1 == 1)
+        assert(t._2 == 2)
+        assert(t._3 == 3)
+        assert(t._4 == 4)
+        assert(t._5 == 5)
+        assert(t._6 == 6)
+        assert(t._7 == 7)
+        assert(t._8 == 8)
     }
 
     @Test
     fun pair() {
+        val p = tuple(1, 2).pair()
+        assert(p.first == 1)
+        assert(p.second == 2)
     }
 
     @Test
-    fun tuple9() {
+    fun tupleOfPair() {
+        val t = Pair(1, 2).tuple()
+        assert(t._1 == 1)
+        assert(t._2 == 2)
     }
 
     @Test
-    fun sequence() {
+    fun tupleOfEntry() {
+        val e: Map.Entry<Int, Int> = mapOf(Pair(1, 2)).entries.iterator().next()
+        val t = e.tuple()
+        assert(t._1 == 1)
+        assert(t._2 == 2)
     }
 
     @Test
     fun sequence1() {
+        val t1 = tuple(1)
+        val t2 = tuple(2)
+        val t: Tuple1<Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
     }
 
     @Test
     fun sequence2() {
+        val t1 = tuple(1, 2)
+        val t2 = tuple(2, 3)
+        val t: Tuple2<Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
     }
 
     @Test
     fun sequence3() {
+        val t1 = tuple(1, 2, 3)
+        val t2 = tuple(2, 3, 4)
+        val t: Tuple3<Seq<Int>, Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
+        assert(t._3 == list(3, 4))
     }
 
     @Test
     fun sequence4() {
+        val t1 = tuple(1, 2, 3, 4)
+        val t2 = tuple(2, 3, 4, 5)
+        val t: Tuple4<Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
+        assert(t._3 == list(3, 4))
+        assert(t._4 == list(4, 5))
     }
 
     @Test
     fun sequence5() {
+        val t1 = tuple(1, 2, 3, 4, 5)
+        val t2 = tuple(2, 3, 4, 5, 6)
+        val t: Tuple5<Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
+        assert(t._3 == list(3, 4))
+        assert(t._4 == list(4, 5))
+        assert(t._5 == list(5, 6))
     }
 
     @Test
     fun sequence6() {
+        val t1 = tuple(1, 2, 3, 4, 5, 6)
+        val t2 = tuple(2, 3, 4, 5, 6, 7)
+        val t: Tuple6<Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
+        assert(t._3 == list(3, 4))
+        assert(t._4 == list(4, 5))
+        assert(t._5 == list(5, 6))
+        assert(t._6 == list(6, 7))
     }
 
     @Test
     fun sequence7() {
+        val t1 = tuple(1, 2, 3, 4, 5, 6, 7)
+        val t2 = tuple(2, 3, 4, 5, 6, 7, 8)
+        val t: Tuple7<Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
+        assert(t._3 == list(3, 4))
+        assert(t._4 == list(4, 5))
+        assert(t._5 == list(5, 6))
+        assert(t._6 == list(6, 7))
+        assert(t._7 == list(7, 8))
+    }
+
+    @Test
+    fun sequence8() {
+        val t1 = tuple(1, 2, 3, 4, 5, 6, 7, 8)
+        val t2 = tuple(2, 3, 4, 5, 6, 7, 8, 9)
+        val t: Tuple8<Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>, Seq<Int>> = list(t1, t2).sequence()
+        assert(t._1 == list(1, 2))
+        assert(t._2 == list(2, 3))
+        assert(t._3 == list(3, 4))
+        assert(t._4 == list(4, 5))
+        assert(t._5 == list(5, 6))
+        assert(t._6 == list(6, 7))
+        assert(t._7 == list(7, 8))
+        assert(t._8 == list(8, 9))
+    }
+
+    @Test
+    fun components2() {
+        val (v1, v2) = tuple(1, 2)
+        assert(v1 == 1)
+        assert(v2 == 2)
+    }
+
+    @Test
+    fun components3() {
+        val (v1, v2, v3) = tuple(1, 2, 3)
+        assert(v1 == 1)
+        assert(v2 == 2)
+        assert(v3 == 3)
+    }
+
+    @Test
+    fun components4() {
+        val (v1, v2, v3, v4) = tuple(1, 2, 3, 4)
+        assert(v1 == 1)
+        assert(v2 == 2)
+        assert(v3 == 3)
+        assert(v4 == 4)
+    }
+
+    @Test
+    fun components5() {
+        val (v1, v2, v3, v4, v5) = tuple(1, 2, 3, 4, 5)
+        assert(v1 == 1)
+        assert(v2 == 2)
+        assert(v3 == 3)
+        assert(v4 == 4)
+        assert(v5 == 5)
+    }
+
+    @Test
+    fun components6() {
+        val (v1, v2, v3, v4, v5, v6) = tuple(1, 2, 3, 4, 5, 6)
+        assert(v1 == 1)
+        assert(v2 == 2)
+        assert(v3 == 3)
+        assert(v4 == 4)
+        assert(v5 == 5)
+        assert(v6 == 6)
+    }
+
+    @Test
+    fun components7() {
+        val (v1, v2, v3, v4, v5, v6, v7) = tuple(1, 2, 3, 4, 5, 6, 7)
+        assert(v1 == 1)
+        assert(v2 == 2)
+        assert(v3 == 3)
+        assert(v4 == 4)
+        assert(v5 == 5)
+        assert(v6 == 6)
+        assert(v7 == 7)
+    }
+
+    @Test
+    fun components8() {
+        val (v1, v2, v3, v4, v5, v6, v7, v8) = tuple(1, 2, 3, 4, 5, 6, 7, 8)
+        assert(v1 == 1)
+        assert(v2 == 2)
+        assert(v3 == 3)
+        assert(v4 == 4)
+        assert(v5 == 5)
+        assert(v6 == 6)
+        assert(v7 == 7)
+        assert(v8 == 8)
     }
 }
