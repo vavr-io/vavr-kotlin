3e09adad1cec8c31fe86d82e185285da564a2612
Ruslan Sennov <ruslan.sennov@gmail.com>
2019-10-09 07:23:34 +0300

    next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "io.vavr"
-version = "0.10.2"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile("io.vavr:vavr:0.10.2")
+    compile("io.vavr:vavr:1.0.0-SNAPSHOT")
     testCompile("junit:junit:4.12")
 }
 
